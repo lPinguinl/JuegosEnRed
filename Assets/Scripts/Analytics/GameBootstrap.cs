d41e0e96a3a092eba2122301bc5210c5389cad62
Enzo Bellini <164093853+lPinguinl@users.noreply.github.com>
2025-12-14 19:56:13 -0300

    changes

--- Assets/Scripts/Analytics/GameBootstrap.cs
+++ Assets/Scripts/Analytics/GameBootstrap.cs
@@ -1,13 +1,11 @@
 using UnityEngine;
-using Unity.Services.Analytics;
 using Unity.Services.Core;
-
 
 public class GameBootstrap : MonoBehaviour
 {
-    private void Awake()
+    private async void Awake()
     {
-        UnityServices.InitializeAsync();
         DontDestroyOnLoad(gameObject);
+        await UnityServices.InitializeAsync();
     }
 }
