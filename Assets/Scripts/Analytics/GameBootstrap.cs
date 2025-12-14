273fa753daf7dc7942c15bd62878bb395a6c55ed
Enzo Bellini <164093853+lPinguinl@users.noreply.github.com>
2025-12-14 18:07:59 -0300

    Final Corrections
    Problems with UnityAanaltycis

--- Assets/Scripts/Analytics/GameBootstrap.cs
+++ Assets/Scripts/Analytics/GameBootstrap.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using Unity.Services.Analytics;
+using Unity.Services.Core;
+
 
 public class GameBootstrap : MonoBehaviour
 {
     private void Awake()
     {
+        UnityServices.InitializeAsync();
         DontDestroyOnLoad(gameObject);
     }
 }
